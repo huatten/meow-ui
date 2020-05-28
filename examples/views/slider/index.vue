479ad9a1bedfb2db48d02d554f9901e459355319
MrTenger <tengjinhua@yunjingkeji.net>
2020-05-28 11:26:57 +0800

    style: update  demos and styles

--- examples/views/slider/index.vue
+++ examples/views/slider/index.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="demo-slider">
-    <div class="mw-field">
-      <p>基本</p>
+  <div class="slider">
+    <section>
+      <h5>基本</h5>
       <mw-slider v-model="value1"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>禁用</p>
+    </section>
+    <section>
+      <h5>禁用</h5>
       <mw-slider v-model="value2" disabled></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>自定义轨道高度</p>
+    </section>
+    <section>
+      <h5>自定义轨道高度</h5>
       <mw-slider v-model="value3" bar-height="3"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>自定义颜色</p>
+    </section>
+    <section>
+      <h5>自定义颜色</h5>
       <mw-slider v-model="value4" active-color="#f00" unactive-color="#ccc"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>指定步长</p>
+    </section>
+    <section>
+      <h5>指定步长</h5>
       <mw-slider v-model="value5" :step="10"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>指定范围边界</p>
+    </section>
+    <section>
+      <h5>指定范围边界</h5>
       <mw-slider v-model="value6" :min="-40" :max="60"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>隐藏tooltip</p>
+    </section>
+    <section>
+      <h5>隐藏tooltip</h5>
       <mw-slider v-model="value7" :showtip="false"></mw-slider>
-    </div>
-    <div class="mw-field">
-      <p>自定义滑块</p>
-      <mw-slider v-model="value8" @change="onChange"><span slot="handle" class="bar">{{value8}}</span></mw-slider>
-    </div>
+    </section>
+    <section>
+      <h5>自定义滑块</h5>
+      <mw-slider v-model="value8" @change="onChange">
+        <span slot="handle" class="bar">{{value8}}</span>
+      </mw-slider>
+    </section>
   </div>
 </template>
 
@@ -46,35 +48,39 @@ export default {
       value5: 10,
       value6: 22,
       value7: 60,
-      value8: 40,
+      value8: 40
     };
   },
-  methods:{
-    onChange(){
-      this.$toast('当前的值是：' + this.value7)
+  methods: {
+    onChange() {
+      this.$toast("当前的值是：" + this.value7);
     }
   }
 };
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
-.mw-field {
-  margin: 20px 18px;
+.slider {
+  padding: 20px 0;
+  margin: 0 30px;
 }
-p {
-  color: #666;
-  font-size: 14px;
-  margin-bottom: 16px;
+section {
+  margin-bottom: 55px;
+  h5 {
+    margin-bottom: 30px;
+    color: rgba(69, 90, 100, 0.6);
+    font-size: 30px;
+  }
 }
-.bar{
-  width: 30px;
-  height: 20px;
+.bar {
+  width: 40px;
+  height: 40px;
   display: block;
   background-color: #f00;
-  border-radius: 10px;
+  border-radius: 30px;
   color: #fff;
   text-align: center;
-  line-height: 20px;
-  font-size: 14px;
+  line-height: 40px;
+  font-size: 24px;
 }
 </style>
--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     'postcss-pxtorem': {
       rootValue: 100, //结果为：设计稿元素尺寸100分之一，比如元素宽750px,最终页面会换算成 7.5rem
-      propList: ['*', '!border-width', '!border-top-width', '!border-right-width', '!border-bottom-width', '!border-left-width'],
+      propList: ['*', '!border-width', '!border', '!border-top-width', '!border-right-width', '!border-bottom-width', '!border-left-width'],
       exclude: /node_modules/i
     }
   }
