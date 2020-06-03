5b6d2c958e6eb1ab492f08080e210c08e90736ac
MrTenger <tengjinhua@yunjingkeji.net>
2020-06-03 19:15:05 +0800

    feat: complete action-sheet and demo

--- examples/views/action-sheet/index.vue
+++ examples/views/action-sheet/index.vue
@@ -1,8 +1,51 @@
 <template>
   <div class="action-sheet">
     <section>
-      <mw-button type="info" :inline="false" @click="show=true">基本</mw-button>
-      <mw-action-sheet v-model="show"></mw-action-sheet>
+      <mw-button type="info" :inline="false" @click="demo1.show=true">基本</mw-button>
+      <mw-action-sheet v-model="demo1.show" :actions="demo1.actions" @selected="selected"></mw-action-sheet>
+    </section>
+    <section>
+      <mw-button type="info" :inline="false" @click="demo2.show=true">安卓风格</mw-button>
+      <mw-action-sheet
+        v-model="demo2.show"
+        :actions="demo2.actions"
+        :position="demo2.position"
+        @selected="selected"
+      ></mw-action-sheet>
+    </section>
+    <section>
+      <mw-button type="info" :inline="false" @click="demo3.show=true">显示取消按钮</mw-button>
+      <mw-action-sheet
+        v-model="demo3.show"
+        :show-cancel="demo3.showCancel"
+        :actions="demo3.actions"
+        @selected="selected"
+      ></mw-action-sheet>
+    </section>
+    <section>
+      <mw-button type="info" :inline="false" @click="demo4.show=true">显示选项状态</mw-button>
+      <mw-action-sheet
+        v-model="demo4.show"
+        :show-cancel="demo4.showCancel"
+        :actions="demo4.actions"
+        @selected="selected"
+      ></mw-action-sheet>
+    </section>
+    <section>
+      <mw-button type="info" :inline="false" @click="demo5.show=true">使用header插槽</mw-button>
+      <mw-action-sheet
+        v-model="demo5.show"
+        :show-cancel="demo5.showCancel"
+        :actions="demo5.actions"
+        @selected="$_delete"
+      >
+        <template slot="header">
+          <div class="header">
+            <h2>确定吗？</h2>
+            <p>删除后就无法找回了呀</p>
+          </div>
+        </template>
+      </mw-action-sheet>
     </section>
   </div>
 </template>
@@ -11,10 +54,109 @@
 export default {
   data() {
     return {
-      show: false
+      demo1: {
+        show: false,
+        actions: [
+          {
+            label: "设置备注和标签",
+            value: 0
+          },
+          {
+            label: "把他推荐给朋友",
+            value: 1
+          },
+          {
+            label: "加入黑名单",
+            value: 2
+          }
+        ]
+      },
+      demo2: {
+        show: false,
+        position: "center",
+        actions: [
+          {
+            label: "设置备注和标签",
+            value: 0
+          },
+          {
+            label: "把他推荐给朋友",
+            value: 1
+          },
+          {
+            label: "加入黑名单",
+            value: 2
+          }
+        ]
+      },
+      demo3: {
+        show: false,
+        showCancel: true,
+        actions: [
+          {
+            label: "设置备注和标签",
+            value: 0
+          },
+          {
+            label: "把他推荐给朋友",
+            value: 1
+          },
+          {
+            label: "加入黑名单",
+            value: 2
+          }
+        ]
+      },
+      demo4: {
+        show: false,
+        showCancel: true,
+        actions: [
+          {
+            label: "设置备注和标签",
+            value: 0
+          },
+          {
+            label: "把他推荐给朋友",
+            value: 1,
+            loading: true
+          },
+          {
+            label: "加入黑名单",
+            value: 2,
+            disabled: true
+          },
+          {
+            label: "删除",
+            value: 3,
+            color: "#f00"
+          }
+        ]
+      },
+      demo5: {
+        show: false,
+        showCancel: true,
+        actions: [
+          {
+            label: "删除",
+            value: "del",
+            color: "#f00"
+          }
+        ]
+      }
     };
   },
-  methods: {}
+  methods: {
+    selected(item) {
+      this.$dialog({
+        content: `${JSON.stringify(item)}`
+      });
+    },
+    $_delete(item){
+      if (item.value === "del") {
+        this.$toast("删除成功");
+      }
+    }
+  }
 };
 </script>
 
@@ -26,5 +168,18 @@ h5 {
   font-size: 30px;
   color: rgba(69, 90, 100, 0.6);
   margin: 30px 0 30px 20px;
+}
+.header {
+  text-align: center;
+  padding: 24px 0;
+  h2 {
+    font-size: 34px;
+    font-weight: 500;
+    padding-bottom: 10px;
+  }
+  p {
+    font-size: 26px;
+    color: #666;
+  }
 }
 </style>
--- packages/action-sheet/action-sheet.vue
+++ packages/action-sheet/action-sheet.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="mw-action-sheet">
     <mw-popup
-      :value="value"
+      v-model="isShow"
       :maskClosable="maskClosable"
       :position="position"
       @input="onInput"
       @open="onShow"
-      @opened="onShowed"
       @close="onHide"
-      @closed="onHided"
     >
-      <div class="mw-action-sheet-content">
-        <div class="mw-action-sheet-header"></div>
-        <ul class="mw-action-sheet-list"></ul>
+      <div class="mw-action-sheet-content" :class="position">
+        <slot name="header" v-if="$slots.header"></slot>
+        <div class="mw-action-sheet-header mw-hairline--bottom" v-if="title">{{title}}</div>
+        <ul class="mw-action-sheet-list">
+          <template v-for="(item, index) in actions">
+            <li
+              :key="index"
+              :class="[
+                'mw-action-sheet-item',
+                (index > 0 || actions.length < 2 ) && 'mw-hairline--top', 
+                item.className,
+                index === clickIndex && 'active', 
+                (index === invalidIndex || item.disabled) && 'disabled', 
+                item.loading && 'loading'
+              ]"
+              :style="{color: item.color}"
+              @click="onSelect(item, index)"
+            >
+              <div class="mw-action-sheet-item-loading" v-if="item.loading">
+                <mw-loading fill="#c8c9cc"></mw-loading>
+              </div>
+              <div class="mw-action-sheet-item-select" v-else v-html="item.text || item.label"></div>
+            </li>
+          </template>
+          <li class="mw-action-sheet-cancel" v-if="showCancel" @click="onCancel">{{ cancelText }}</li>
+        </ul>
       </div>
     </mw-popup>
   </div>
@@ -20,6 +41,7 @@
 
 <script type="text/ecmascript-6">
 import MPopup from "../popup";
+import MLoading from "../loading";
 export default {
   name: "mw-action-sheet",
   props: {
@@ -28,15 +50,32 @@ export default {
       type: Boolean,
       default: false
     },
-    theme: {
-      //主题 默认ios风格
+    position: {
+      //默认底部弹出
       type: String,
-      default: "ios"
+      default: "bottom"
     },
     title: {
       //窗口标题
       type: String,
       default: ""
+    },
+    actions: {
+      //操作选项
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    defaultIndex: {
+      //默认选中项
+      type: Number,
+      default: -1
+    },
+    invalidIndex: {
+      //禁用选项
+      type: Number,
+      default: -1
     },
     closable: {
       //是否显示关闭按钮
@@ -45,6 +84,11 @@ export default {
     },
     maskClosable: {
       //弹出层是否可关闭
+      type: Boolean,
+      default: true
+    },
+    showCancel: {
+      //是否显示取消
       type: Boolean,
       default: false
     },
@@ -58,31 +102,24 @@ export default {
       type: String,
       default: ""
     },
-    confirmText: {
-      //确定按钮文案
-      type: String,
-      default: "确认"
-    },
-    confirmColor: {
-      //确定按钮颜色
-      type: String,
-      default: ""
-    },
     lockScroll: {
       //是否锁定背景滚动
       type: Boolean,
       default: true
     }
   },
-  data() {
-    return {};
-  },
-  computed: {
-    position() {
-      return this.theme === "ios" ? "bottom" : "center";
+  watch: {
+    value(newVal) {
+      this.isShow = newVal;
     }
   },
-  components: { MPopup },
+  data() {
+    return {
+      isShow: this.value,
+      clickIndex: -1
+    };
+  },
+  components: { MPopup, MLoading },
   methods: {
     onInput(value) {
       this.$emit("input", value);
@@ -90,23 +127,29 @@ export default {
     onShow() {
       this.$emit("open");
     },
-    onShowed() {
-      this.$emit("opened");
-    },
     onHide() {
       this.$emit("close");
     },
-    onHided() {
-      this.$emit("closed");
+    onSelect(item, index) {
+      if (index === this.invalidIndex || item.disabled || item.loading) {
+        return;
+      }
+      this.clickIndex = index;
+      this.$emit("selected", item);
+      this.hideSheet();
+    },
+    onCancel() {
+      this.$emit("cancel");
+      this.hideSheet();
+    },
+    hideSheet() {
+      this.isShow = false;
+      this.$emit("input", false);
     }
   }
 };
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
-.mw-action-sheet {
-  &-content {
-    background-color: #fff;
-  }
-}
+
 </style>
