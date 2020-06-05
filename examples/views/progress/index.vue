52efbd6535c3400a398b41ef731bcea26b1f67ba
MrTenger <tengjinhua@yunjingkeji.net>
2020-06-05 17:08:59 +0800

    feat: complete progress component and demo

--- examples/views/progress/index.vue
+++ examples/views/progress/index.vue
@@ -1,8 +1,58 @@
 <template>
   <div class="progress">
     <section>
-      <h5>默认用法</h5>
-      <mw-progress v-model="width"></mw-progress>
+      <h5>基本用法</h5>
+      <mw-progress v-model="base.value"></mw-progress>
+    </section>
+    <section>
+      <h5>设置高度和百分比大小</h5>
+      <mw-progress v-model="height.value" :height="height.height" :percentSize="height.fontSize"></mw-progress>
+    </section>
+    <section>
+      <h5>设置圆角进度条</h5>
+      <mw-progress v-model="circle.value" :round="circle.round"></mw-progress>
+    </section>
+    <section>
+      <h5>更改进度条外观</h5>
+      <mw-progress
+        v-model="theme.value"
+        :activeColor="theme.activeColor"
+        :unactiveColor="theme.unactiveColor"
+      ></mw-progress>
+    </section>
+    <section>
+      <h5>开启进度条动画</h5>
+      <mw-progress v-model="animate.value" :animate="animate.animate"></mw-progress>
+    </section>
+    <section>
+      <h5>百分比内显并展示标题</h5>
+      <mw-progress
+        v-model="inner.value"
+        :title="inner.title"
+        :height="inner.height"
+        :percent-position="inner.position"
+      ></mw-progress>
+    </section>
+    <section>
+      <h5>百分比上部显示并展示标题</h5>
+      <mw-progress v-model="top.value" :title="top.title" :percent-position="top.position"></mw-progress>
+    </section>
+    <section>
+      <h5>投票</h5>
+      <div class="voteitem" v-for="(item,index) in votes" :key="index">
+        <mw-progress
+          v-model="item.value"
+          :title="item.title"
+          :activeColor="item.activeColor"
+          percent-position="top"
+        ></mw-progress>
+      </div>
+    </section>
+    <section>
+      <h5>修改百分比</h5>
+      <mw-progress v-model="value" round animate percent-position="inner"></mw-progress>
+      <mw-button type="danger" @click="percentMinus">减少</mw-button>
+      <mw-button type="info" @click="percentPlus">增加</mw-button>
     </section>
   </div>
 </template>
@@ -11,25 +61,88 @@
 export default {
   data() {
     return {
-      width: 30
+      base: {
+        value: 40
+      },
+      height: {
+        value: 50,
+        height: 22,
+        fontSize: 16
+      },
+      circle: {
+        value: 55,
+        round: true
+      },
+      theme: {
+        value: 60,
+        activeColor: "#07c160",
+        unactiveColor: "#f3f3f3",
+        animate: true
+      },
+      animate: {
+        value: 50,
+        animate: true
+      },
+      inner: {
+        value: 70,
+        height: 20,
+        title: "Vite",
+        position: "inner"
+      },
+      top: {
+        value: 80,
+        title: "JS",
+        position: "top"
+      },
+      votes: [
+        {
+          title: "Vue",
+          value: 80,
+          activeColor: "#42b983"
+        },
+        {
+          title: "React",
+          value: 90,
+          activeColor: "#61dafb"
+        },
+        {
+          title: "Angular",
+          value: 50,
+          activeColor: "red"
+        }
+      ],
+      value: 10
     };
   },
-  methods: {}
+  methods: {
+    percentPlus() {
+      if (this.value < 100) {
+        this.value += 10;
+      }
+    },
+    percentMinus() {
+      if (this.value > 0) {
+        this.value -= 10;
+      }
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
 .progress {
   background: #fff;
-  padding: 20px 0;
+  padding: 24px;
 }
 section {
-  margin-bottom: 40px;
+  margin-bottom: 50px;
   h5 {
-    margin-bottom: 15px;
-    margin-left: 15px;
+    margin-bottom: 20px;
     color: rgba(69, 90, 100, 0.6);
     font-size: 30px;
+  }
+  .voteitem {
+    margin-bottom: 40px;
   }
 }
 </style>
--- packages/progress/progress.vue
+++ packages/progress/progress.vue
@@ -1,22 +1,89 @@
 <template>
-  <div class="mw-progress">
-    <div class="mw-progress-bar">
-      <div class="mw-progress-bar-inner" :style="{width: barWidth}"></div>
+  <div class="mw-progress" :data-percent="value">
+    <div
+      class="mw-progress-text-top"
+      v-if="showPercent && percentPosition==='top'"
+      :style="{width: barWidth, fontSize: (percentSize*1) + 'px'}"
+    >
+      <span class="title">{{title}}</span>
+      <span class="percent">{{barWidth}}</span>
+    </div>
+    <div class="mw-progress-cont" :class="{'round': round}">
+      <div class="mw-progress-bar" :style="{height: barHeight, background: unactiveColor}">
+        <div
+          class="mw-progress-bar-inner"
+          :class="{'animate': animate}"
+          :style="{width: barWidth, background: activeColor, fontSize: (percentSize*1) + 'px'}"
+        >
+          <template v-if="showPercent && percentPosition==='inner'">
+            <span class="title">{{title}}</span>
+            <span class="percent">{{barWidth}}</span>
+          </template>
+        </div>
+      </div>
+      <div
+        class="mw-progress-text-right"
+        v-if="showPercent && percentPosition==='right'"
+        :style="{fontSize: (percentSize*1) + 'px'}"
+      >{{barWidth}}</div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { getStyle } from "../_util/dom";
 export default {
   name: "mw-progress",
   props: {
     value: {
+      //双向绑定进度条的值
       type: Number,
       default: 0
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+    height: {
+      //进度条高度
+      type: [Number, String],
+      default: 14
+    },
+    round: {
+      //是否显示圆角
+      type: Boolean,
+      default: false
+    },
+    animate: {
+      //是否开启进度条动画
+      type: Boolean,
+      default: false
+    },
+    activeColor: {
+      //进度条颜色
+      type: String,
+      default: "#ff8200"
+    },
+    unactiveColor: {
+      //进度条底部轨道颜色
+      type: String,
+      default: "#ebebeb"
+    },
+    showPercent: {
+      //是否显示百分比
+      type: Boolean,
+      default: true
+    },
+    percentPosition: {
+      //百分比显示位置 top/inner/right
+      type: String,
+      default: "right"
+    },
+    percentSize: {
+      //百分比文字大小
+      type: [String, Number],
+      default: 14
     }
-  },
-  data() {
-    return {};
   },
   watch: {
     value: {
@@ -29,6 +96,12 @@ export default {
   computed: {
     barWidth() {
       return this.value + "%";
+    },
+    barHeight() {
+      return this.height + "px";
+    },
+    textTop() {
+      return -this.height + "px";
     }
   },
   methods: {
@@ -38,20 +111,3 @@ export default {
   }
 };
 </script>
-
-<style  lang="scss" rel="stylesheet/scss">
-.mw-progress {
-  display: flex;
-  align-items: center;
-  &-bar {
-    flex: 1;
-    height: 12px;
-    background-color: #ebebeb;
-  }
-  &-bar-inner {
-    height: 100%;
-    transition: width 0.4s;
-    background-color: #f82;
-  }
-}
-</style>
