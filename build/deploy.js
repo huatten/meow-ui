38930f68600a516c4f8a23b4832b094eb712b39c
huatten <nbilly@163.com>
2024-12-27 11:22:37 +0800

    update deploy

--- build/deploy.js
+++ build/deploy.js
@@ -1,9 +1,15 @@
 const ghpages = require('gh-pages');
+const path = require('path');
 
-
-ghpages.publish('./demo', {
+// 部署配置
+ghpages.publish(path.resolve(__dirname, '../demo'), {
   branch: 'gh-pages',
-  message: "deploy: examples site",
-}, function () {
-  console.log('同步gh-pages成功!');
+  message: 'deploy: examples site',
+  repo: `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+}, function (err) {
+  if (err) {
+    console.error('部署失败:', err);
+  } else {
+    console.log('同步到 gh-pages 分支成功！');
+  }
 });
