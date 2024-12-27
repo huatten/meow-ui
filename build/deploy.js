e553d2489051cbdcacd4ed18752e65a2a50e595a
huatten <nbilly@163.com>
2024-12-27 11:29:09 +0800

    update deploy

--- build/deploy.js
+++ build/deploy.js
@@ -1,11 +1,14 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 
-// 部署配置
 ghpages.publish(path.resolve(__dirname, '../demo'), {
   branch: 'gh-pages',
   message: 'deploy: examples site',
   repo: `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+  user: {
+    name: process.env.GITHUB_ACTOR || 'github-actions', // 使用 GitHub Actions 默认的 GITHUB_ACTOR
+    email: 'github-actions@github.com',                // 使用 GitHub 提供的通用邮箱
+  },
 }, function (err) {
   if (err) {
     console.error('部署失败:', err);
