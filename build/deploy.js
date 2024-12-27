0f8c14aa740b2a6427cf67060e3795985574aebe
huatten <nbilly@163.com>
2024-12-27 14:50:29 +0800

    update deploy.js

--- build/deploy.js
+++ build/deploy.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 
-ghpages.publish(path.resolve(__dirname, '../dist'), {
+ghpages.publish(path.resolve(__dirname, '../demo'), {
   branch: 'gh-pages',
   message: 'deploy: examples site',
   repo: `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
